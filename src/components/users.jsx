76a0bf73e22465b7c3ea641c068ce01aff8fd701
Ruslan <dantess88@mail.ru>
2022-12-24 22:30:42 +0500

    done

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -2,19 +2,8 @@ import React, { useState } from "react";
 import API from "../api";
 
 const Users = () => {
-  console.log(API.users.fetchAll());
   const [users, setUsers] = useState(API.users.fetchAll());
-  const [qualities, setQualities] = useState(
-    users.map((user) => {
-      return user.qualities;
-    })
-  );
-  console.log(qualities);
-  const qualitiesNames = qualities.map((el) => {
-    return el.length;
-  });
-  console.log(qualitiesNames);
-  // console.log(users.length, users[6].name);
+
   const handleDelete = (userId) => {
     setUsers((prevState) => prevState.filter((user) => user !== userId));
   };
@@ -28,12 +17,6 @@ const Users = () => {
     return howManyHumans;
   };
 
-  // const renderTable = () => {if (users.length === 0) {
-  //   <div/>
-  // } else {
-
-  // }
-
   return (
     <>
       {users.length === 0 ? (
@@ -43,45 +26,52 @@ const Users = () => {
           users.length
         )} тусанет с тобой`}</h1>
       )}
-      {/*можно переписать через функцию, ну уж ладно */}
-      
+
       {users.length > 0 ? (
         <table className="table">
-        <thead>
-          <tr>
-            <th>Имя</th>
-            <th>Качества</th>
-            <th>Профессия</th>
-            <th>Встретился, раз</th>
-            <th>Оценка</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key="user._id">
-              <td>{user.name}</td>
-              <td>{user._id}</td>
-              <td>{user.profession.name}</td>
-              <td>{user.completedMeetings}</td>
-              <td>{`${user.rate}/5`}</td>
-              <td>
-                <button
-                  className="btn btn-danger"
-                  onClick={() => handleDelete(user)}
-                >
-                  delete
-                </button>
-              </td>
+          <thead>
+            <tr>
+              <th>Имя</th>
+              <th>Качества</th>
+              <th>Профессия</th>
+              <th>Встретился, раз</th>
+              <th>Оценка</th>
+              <th></th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key="user._id">
+                <td>{user.name}</td>
+                <td>
+                  {user.qualities.map((el) => {
+                    return (
+                      <>
+                        <span className={`badge m-2 bg-${el.color}`}>
+                          {el.name}
+                        </span>
+                      </>
+                    );
+                  })}
+                </td>
+                <td>{user.profession.name}</td>
+                <td>{user.completedMeetings}</td>
+                <td>{`${user.rate}/5`}</td>
+                <td>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => handleDelete(user)}
+                  >
+                    delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       ) : (
         <div/>
       )}
-
-      
     </>
   );
 };
