0025bf9db8e02f29fd9ae181218256540a29f4be
Ruslan <dantess88@mail.ru>
2023-01-04 14:42:30 +0500

    initial second week (rigth solution)

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,79 +1,74 @@
 import React, { useState } from "react";
-import API from "../api";
+import api from "../api"
 
 const Users = () => {
-  const [users, setUsers] = useState(API.users.fetchAll());
+    const [users, setUsers] = useState(api.users.fetchAll());
 
-  const handleDelete = (userId) => {
-    setUsers((prevState) => prevState.filter((user) => user !== userId));
-  };
-  const renderPhrase = (number) => {
-    let howManyHumans;
-    if (number === 2 || number === 3 || number === 4) {
-      howManyHumans = `человека`;
-    } else {
-      howManyHumans = `человек`;
-    }
-    return howManyHumans;
-  };
+    const handleDelete = (userId) => {
+        setUsers(users.filter((user) => user._id !== userId));
+    };
 
-  return (
-    <>
-      {users.length === 0 ? (
-        <h1 className="badge bg-danger">{"Никто с тобой не тусанет"}</h1>
-      ) : (
-        <h1 className="badge bg-primary">{`${users.length} ${renderPhrase(
-          users.length
-        )} тусанет с тобой`}</h1>
-      )}
+    const renderPhrase = (number) => {
+        const lastOne = Number(number.toString().slice(-1));
+        if (number > 4 && number < 15) return "человек тусанет";
+        if ([2, 3, 4].indexOf(lastOne) >= 0) return "человека тусанут";
+        if (lastOne === 1) return "человек тусанет";
+        return "человек тусанет";
+    };
 
-      {users.length > 0 ? (
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Имя</th>
-              <th>Качества</th>
-              <th>Профессия</th>
-              <th>Встретился, раз</th>
-              <th>Оценка</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map((user) => (
-              <tr key="user._id">
-                <td>{user.name}</td>
-                <td>
-                  {user.qualities.map((el) => {
-                    return (
-                      <>
-                        <span className={`badge m-2 bg-${el.color}`}>
-                          {el.name}
-                        </span>
-                      </>
-                    );
-                  })}
-                </td>
-                <td>{user.profession.name}</td>
-                <td>{user.completedMeetings}</td>
-                <td>{`${user.rate}/5`}</td>
-                <td>
-                  <button
-                    className="btn btn-danger"
-                    onClick={() => handleDelete(user)}
-                  >
-                    delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      ) : (
-        <div/>
-      )}
-    </>
-  );
+    return (
+        <>
+            <h2>
+                <span
+                    className={"badge " + (users.length > 0 ? "bg-primary" : "bg-danger")}
+                >
+                    {users.length > 0
+                        ? `${users.length + " " + renderPhrase(users.length)} с тобой сегодня`
+                        : "Никто с тобой не тусанет"}
+                </span>
+            </h2>
+
+            {users.length > 0 && (
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Имя</th>
+                            <th scope="col">Качества</th>
+                            <th scope="col">Профессия</th>
+                            <th scope="col">Встретился, раз</th>
+                            <th scope="col">Оценка</th>
+                            <th />
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {users.map((user) => (
+                            <tr key={user._id}>
+                            <td>{user.name}</td>
+                            <td>
+                                {user.qualities.map((item) => (
+                                    <span className={"badge m-1 bg-" + item.color} key={item._id}>
+                                        {item.name}
+                                    </span>
+                                ))}
+                            </td>
+                            <td>{user.profession.name}</td>
+                            <td>{user.completedMeetings}</td>
+                            <td>{user.rate} /5</td>
+                            <td>
+                                <button
+                                    onClick={() => handleDelete(user._id)}
+                                    className="btn btn-danger"
+                                >
+                                    delete
+                                </button>
+                            </td>
+                        </tr>
+                        ))}
+                    </tbody>
+                </table>
+            )}
+        </>
+    );
 };
 
 export default Users;
