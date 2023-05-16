51f08fde9aa96b034959e2178c142dd3b559b092
Ruslan <dantess88@mail.ru>
2023-05-16 13:01:19 +0500

    13ый урок

--- src/app/components/page/userPage/userPage.jsx
+++ src/app/components/page/userPage/userPage.jsx
@@ -1,36 +1,40 @@
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import api from "../../../api";
-import Qualities from "../../ui/qualities";
-import { useHistory } from "react-router-dom";
+import UserCard from "../../ui/userCard";
+import MeetingsCard from "../../ui/meetingsCard";
+import QualitiesCard from "../../ui/qualitiesCard";
+import Comments from "../../ui/comments";
 
 const UserPage = ({ userId }) => {
-    const history = useHistory();
-    const [user, setUser] = useState();
-    useEffect(() => {
-        api.users.getById(userId).then((data) => setUser(data));
-    }, []);
-    const handleClick = () => {
-        history.push(`/users/${userId}/edit`);
-    };
-    if (user) {
-        return (
-            <div>
-                <h1> {user.name}</h1>
-                <h2>Профессия: {user.profession.name}</h2>
-                <Qualities qualities={user.qualities} />
-                <p>completedMeetings: {user.completedMeetings}</p>
-                <h2>Rate: {user.rate}</h2>
-                <button onClick={handleClick}>Изменить информацию о пользователе</button>
-            </div>
-        );
-    } else {
-        return "loading...";
-    }
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    api.users.getById(userId).then((data) => setUser(data));
+  }, []);
+
+  if (user) {
+    return (
+      <div className="container">
+        <div className="row gutters-sm">
+          <div className="col-md-4 mb-3">
+            <UserCard user={user} />
+            <QualitiesCard data={user.qualities} />
+            <MeetingsCard value={user.completedMeetings} />
+          </div>
+          <div className="col-md-8">
+            <Comments />
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return "loading...";
+  }
 };
 
 UserPage.propTypes = {
-    userId: PropTypes.string.isRequired
+  userId: PropTypes.string.isRequired
 };
 
 export default UserPage;
